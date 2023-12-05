994b8b9b1caf9243cd31943f08e6af89170564bd
ivanhoe-genius <ivanhoe_genius@yahoo.com>
2023-12-05 01:12:48 +0100

    feat: create playRound function

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
 function getComputerChoice() {
     const computerChoices = ['Rock', 'Paper', 'Scissors'];
-    return computerChoices[Math.floor(Math.random() * 3)]
+    return computerChoices[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice())
+function getPlayerChoice() {
+    let playerSelection = prompt('Rock, Paper or Scissors?');
+
+    if (playerSelection.trim().toLowerCase() !== 'rock' &&
+        playerSelection.trim().toLowerCase() !== 'paper' &&
+        playerSelection.trim().toLowerCase() !== 'scissors') {
+        console.log(' Check your spelling! Rock, Paper or Scissors?');
+        getPlayerChoice();
+    } else {
+        return playerSelection.trim().toLowerCase();
+    }
+}
+
+
+
+let playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection);
+    console.log(computerSelection);
+
+    let result;
+    switch (true) {
+        case playerSelection === 'rock' && computerSelection === 'Paper':
+            result = 'You Lose! Paper beats Rock.';
+            break;
+        case playerSelection === 'rock' && computerSelection === 'Scissors':
+            result = 'You Win! Rock beats Scissors.';
+            break;
+        case playerSelection === 'paper' && computerSelection === 'Rock':
+            result = 'You Win! Paper beats Rock.';
+            break;
+        case playerSelection === 'paper' && computerSelection === 'Scissors':
+            result = 'You Lose! Scissors beat Paper.';
+            break;
+        case playerSelection === 'scissors' && computerSelection === 'Rock':
+            result = 'You Lose! Rock beats Scissors.';
+            break;
+        case playerSelection === 'scissors' && computerSelection === 'Paper':
+            result = 'You Win! Scissors beat Paper.';
+            break;
+        default:
+            result = 'Tie! Nobody wins.';
+            break;
+    }
+    return result;
+}
+
+
+console.log(playRound(playerSelection, computerSelection));
